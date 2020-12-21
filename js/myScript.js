a8aa22b0d5671de19126e244eb53a5212e3a64b8
Mustafa Tatlisu <tatlisumustafa3@gmail.com>
2020-12-21 12:09:05 +0300

    NavBar pt.2
    Navbar finalized on scroll fixk to top, filled in navbar links, added margin(class: content-buffer) to text when screen is small so ocntent is edge to edge when orientation goes to stacked.

--- js/myScript.js
+++ js/myScript.js
@@ -1,9 +1,5 @@
 
-//  alert("This alert box was called with the onload event");
-
-
 $(document).ready(function () {
-
   $('.first-button').on('click', function () {
 
     $('.animated-icon1').toggleClass('open');
@@ -16,4 +12,24 @@ $(document).ready(function () {
 
     $('.animated-icon3').toggleClass('open');
   });
+});
+document.documentElement.clientHeight
+//  alert("This alert box was called with the onload event");
+$(document).scroll(function() {  //alert("This alert box was called with the onload event");
+    var scrollTop = document.documentElement.clientHeight-71;
+  //  console.log("---");
+  //  console.log(document.documentElement.clientHeight)
+    if($(window).scrollTop() >= scrollTop){
+      document.getElementById("make-fixed").classList.remove("margin-start");
+
+document.getElementById("make-fixed").classList.add("fixed-top");
+    if($(window).scrollTop() >= scrollTop){
+document.getElementById("make-fixed").classList.add("scrolling-navbar");
+}
+} else {
+  //    alert("This alert bo11111the onload event");
+        document.getElementById("make-fixed").classList.remove("fixed-top");
+        document.getElementById("make-fixed").classList.add("margin-start");
+        document.getElementById("make-fixed").classList.remove("scrolling-navbar");
+    }
 });
